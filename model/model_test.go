423cc1fc48d51991480cf35a767399c28ba02d1b
Nguyen Duc Toan <ntoan96@gmail.com>
2025-02-19 21:26:39 +0700

    fix test

--- model/model_test.go
+++ model/model_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestBreadCrumbItems(t *testing.T) {
 	model := FilesPageModel{Path: "./a/b/c/d"}
-	items := model.Parents()
+	items := model.Path.Parents()
 	assert.Equal(t, items[0].Name, "a", "should be equal")
 	assert.Equal(t, items[1].Name, "b", "should be equal")
 	assert.Equal(t, items[2].Name, "c", "should be equal")
-	assert.Equal(t, items[0].Path, "a", "should be equal")
-	assert.Equal(t, items[1].Path, "a/b", "should be equal")
-	assert.Equal(t, items[2].Path, "a/b/c", "should be equal")
+	assert.Equal(t, items[0].Path, Path("a"), "should be equal")
+	assert.Equal(t, items[1].Path, Path("a/b"), "should be equal")
+	assert.Equal(t, items[2].Path, Path("a/b/c"), "should be equal")
 }
